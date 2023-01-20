178a4dba7ea95339919cfc6fe2d76e07037b8a08
AlfieLamerton <alfie2.lamerton@live.uwe.ac.uk>
2023-01-20 17:29:53 +0000

    Added colour theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import React from "react";
-import { Box, CssBaseline, Paper, Typography } from "@mui/material";
+import { Box, CssBaseline, Paper, Typography, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+
 
 function App() {
+
+   // define theme
+   const theme = createTheme({
+    palette: {
+      primary: {
+        light: "#6a7b83",
+        main: "#455a64",
+        dark: "#303e46",
+        contrastText: "#000",
+      },
+      secondary: {
+        main: "#4db6ac",
+        light: "#b5a133",
+        dark: "#726000",
+        contrastText: "#000",
+      },
+    },
+  });
+
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Box
         height="100vh"
@@ -21,7 +42,7 @@ function App() {
           </Typography>
         </Paper>
       </Box>
-    </div>
+    </ThemeProvider>
   );
 }
 
