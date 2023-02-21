6833b9427cf379202b619ff6566ec444d60b7676
AlfieLamerton <alfie2.lamerton@live.uwe.ac.uk>
2023-02-21 10:48:09 +0000

    Added first image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,23 @@
-import React, {ReactElement, FC} from "react";
-import {Box, Typography} from "@mui/material";
+import React, { ReactElement, FC } from "react";
+import { Box, Typography } from "@mui/material";
 
 const Home: FC<any> = (): ReactElement => {
-    return (
-        <Box sx={{
-            flexGrow: 1,
-            backgroundColor: 'whitesmoke',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-        }}>
-            <Typography variant="h3">Home</Typography>
-        </Box>
-    );
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        backgroundColor: "whitesmoke",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <img
+        src={process.env.PUBLIC_URL + "london-2374247with pink.jpg"}
+        alt="cannot display"
+      />
+    </Box>
+  );
 };
 
 export default Home;
