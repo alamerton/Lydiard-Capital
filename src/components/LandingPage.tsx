4decedad680fd584b674f4d8875034ee61a1701e
AlfieLamerton <alfie2.lamerton@live.uwe.ac.uk>
2023-02-23 13:55:28 +0000

    Added wrapper div around landing page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -4,29 +4,33 @@ import { Component } from "react";
 class TopImage extends Component {
   render() {
     return (
-      <div className="first-image-with-text" style={{ position: "relative" }}>
-        <img
-          style={{ width: "100%" }}
-          src={process.env.PUBLIC_URL + "london-2374247with pink.jpg"}
-          alt="cannot display"
-        />
-        <Typography
-          gutterBottom
-          component="div"
-          variant="h4"
-          flexGrow={1}
-          sx={{
-            position: "absolute",
-            bottom: "0.1rem",
-            right: "1rem",
-            fontFamily: "Source Serif 4",
-            fontWeight: "light",
-            textAlign: "right",
-            color: "text.secondary",
-          }}
-        >
-          Creativity Is Capital
-        </Typography>
+    <div className="landing-page">
+        <div className="first-image-with-text" style={{ position: "relative" }}>
+            <img
+            style={{ width: "100%" }}
+            src={process.env.PUBLIC_URL + "london-2374247with pink.jpg"}
+            alt="cannot display"
+            />
+            <Typography
+            gutterBottom
+            component="div"
+            variant="h4"
+            flexGrow={1}
+            sx={{
+                position: "absolute",
+                bottom: "0.1rem",
+                right: "1rem",
+                fontFamily: "Source Serif 4",
+                fontWeight: "light",
+                textAlign: "right",
+                color: "text.secondary",
+            }}
+            >
+            Creativity Is Capital
+            </Typography>
+        </div>
+        <div className="gross-development-value-mission-card">
+        </div>
       </div>
     );
   }
