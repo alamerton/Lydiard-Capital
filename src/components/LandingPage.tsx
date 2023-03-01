27ae2bc25cd1261127648d9655ecaea283c4119a
AlfieLamerton <alfie2.lamerton@live.uwe.ac.uk>
2023-03-01 11:21:06 +0000

    Fixed whitespace between images, shrunk purple bars and added first heading and body

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,13 +1,17 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import { Component } from "react";
 
 class TopImage extends Component {
   render() {
     return (
       <Box>
-        <Box style={{ position: "relative" }}>
+        <Box style={{ position: "relative", marginBottom: "-1rem" }}>
           <img
-            style={{ width: "100%", height: "100%" }}
+            style={{
+              width: "100%",
+              height: "100%",
+              padding: 0,
+            }}
             src={process.env.PUBLIC_URL + "london-2374247with pink.jpg"}
             alt="cannot display"
           />
@@ -17,8 +21,8 @@ class TopImage extends Component {
             flexGrow={1}
             sx={{
               position: "absolute",
-              bottom: "1rem",
               right: "1rem",
+              bottom: "1rem",
               fontFamily: "Source Serif 4",
               fontWeight: "light",
               textAlign: "right",
@@ -28,10 +32,10 @@ class TopImage extends Component {
             Creativity Is Capital
           </Typography>
         </Box>
-        <Box style={{ position: "relative" }}>
-          <Box sx={{ backgroundColor: "secondary.main", height: "0.5rem" }} />
+        <Box style={{ position: "relative", marginBottom: "-1rem" }}>
+          <Box sx={{ backgroundColor: "secondary.main", height: "0.3rem" }} />
           <img
-            style={{ width: "100%", height: "100%" }}
+            style={{ width: "100%", height: "100%", filter: "brightness(85%)" }}
             src={process.env.PUBLIC_URL + "greenfield-brownfield.jpg"}
             alt="cannot display"
           />
@@ -50,14 +54,14 @@ class TopImage extends Component {
               color: "text.secondary",
             }}
           >
-            We Maximise Gross Development Value across both Brownfield and
+            Maximising Gross Development Value across both Brownfield and
             Greenfield land
           </Typography>
         </Box>
-        <Box style={{ position: "relative" }}>
-          <Box sx={{ backgroundColor: "secondary.main", height: "0.5rem" }} />
+        <Box style={{ position: "relative", marginBottom: "-1rem" }}>
+          <Box sx={{ backgroundColor: "secondary.main", height: "0.3rem" }} />
           <img
-            style={{ width: "100%", height: "100%" }}
+            style={{ width: "100%", height: "100%", filter: "brightness(75%)" }}
             src={process.env.PUBLIC_URL + "shard1.jpg"}
             alt="cannot display"
           />
@@ -76,11 +80,11 @@ class TopImage extends Component {
               color: "text.secondary",
             }}
           >
-            We Keep Construction Costs to a Minimum
+            Keeping Construction Costs to a Minimum
           </Typography>
         </Box>
         <Box style={{ position: "relative" }}>
-          <Box sx={{ backgroundColor: "secondary.main", height: "0.5rem" }} />
+          <Box sx={{ backgroundColor: "secondary.main", height: "0.3rem" }} />
           <img
             style={{ width: "100%", height: "100%" }}
             src={process.env.PUBLIC_URL + "buildings-1835643.jpg"}
@@ -92,8 +96,9 @@ class TopImage extends Component {
             flexGrow={1}
             sx={{
               position: "absolute",
-              bottom: "11.5rem",
+              bottom: "11rem",
               right: "1rem",
+              left: "3rem",
               fontFamily: "Source Serif 4",
               fontWeight: "bold",
               fontStyle: "italic",
@@ -101,7 +106,61 @@ class TopImage extends Component {
               color: "text.secondary",
             }}
           >
-            We Design Exceptional Residential-Led Mixed-Use developments
+            Designing Exceptional Residential-Led Mixed-Use developments
+          </Typography>
+        </Box>
+        <Box
+          style={{
+            position: "relative",
+            maxWidth: "80%",
+            margin: "auto",
+            marginTop: "1rem",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "5rem",
+          }}
+        >
+          <Typography
+            component="div"
+            variant="h5"
+            flexGrow={1}
+            width="100%"
+            height="2rem"
+            sx={{
+              position: "absolute",
+              fontFamily: "Roboto",
+              textAlign: "center",
+              color: "text.primary",
+            }}
+          >
+            Be Part of a Ground-Breaking Vision
+          </Typography>
+        </Box>
+        <Box
+          style={{
+            position: "relative",
+            maxWidth: "80%",
+            margin: "auto",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "5rem",
+          }}
+        >
+          <Typography
+            component="div"
+            variant="body1"
+            flexGrow={1}
+            width="100%"
+            height="2rem"
+            sx={{
+              position: "absolute",
+              fontFamily: "Roboto",
+              textAlign: "center",
+              color: "text.primary",
+            }}
+          >
+            We work in partnership with landowners & Investors and navigate the
+            complex UK planning system to secure valuable planning permissions.
           </Typography>
         </Box>
       </Box>
