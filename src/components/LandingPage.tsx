c73b6675fdf23e7382fa0b700eb37aa404ce069b
AlfieLamerton <alfie2.lamerton@live.uwe.ac.uk>
2023-03-03 12:30:44 +0000

    Added Lydiard Capital Commitment and Influence an Experience sections

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -547,6 +547,7 @@ class TopImage extends Component {
             position: "relative",
             maxWidth: "80%",
             margin: "auto",
+            marginBottom: "1rem",
             justifyContent: "center",
             alignItems: "center",
             minHeight: "6rem",
@@ -570,7 +571,265 @@ class TopImage extends Component {
             can.
           </Typography>
         </Box>
-        
+        <Box style={{ position: "relative" }}>
+          <Box sx={{ backgroundColor: "secondary.main", height: "0.3rem" }} />
+          <img
+            style={{ width: "100%", height: "100%" }}
+            src={process.env.PUBLIC_URL + "Group 22.png"}
+            alt="cannot display"
+          />
+          <Typography
+            component="div"
+            variant="h5"
+            flexGrow={1}
+            sx={{
+              position: "absolute",
+              bottom: "1rem",
+              left: "2.75rem",
+              fontFamily: "Source Serif 4",
+              fontWeight: "bold",
+              fontStyle: "italic",
+              textAlign: "left",
+              color: "text.secondary",
+            }}
+          >
+            True Quality Lasts
+          </Typography>
+        </Box>
+        <Box
+          style={{
+            position: "relative",
+            maxWidth: "80%",
+            margin: "auto",
+            marginTop: "1rem",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "5rem",
+          }}
+        >
+          <Typography
+            component="div"
+            variant="h5"
+            flexGrow={1}
+            width="100%"
+            height="2rem"
+            sx={{
+              position: "absolute",
+              fontFamily: "Roboto",
+              textAlign: "center",
+              color: "text.primary",
+            }}
+          >
+            The Lydiard Capital Commitment
+          </Typography>
+        </Box>
+        <Box
+          style={{
+            position: "relative",
+            maxWidth: "80%",
+            margin: "auto",
+            marginBottom: "1rem",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "6rem",
+          }}
+        >
+          <Typography
+            component="div"
+            variant="body1"
+            flexGrow={1}
+            width="100%"
+            height="2rem"
+            sx={{
+              position: "absolute",
+              fontFamily: "Roboto",
+              textAlign: "left",
+              color: "text.primary",
+            }}
+          >
+            We believe architecture is the most important art form. It is where
+            we live, work, shop, and coalesce. Where ideas are formed, and
+            experiences shared.
+          </Typography>
+        </Box>
+        <Box
+          style={{
+            position: "relative",
+            maxWidth: "80%",
+            margin: "auto",
+            marginBottom: "1rem",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "6rem",
+          }}
+        >
+          <Typography
+            component="div"
+            variant="body1"
+            flexGrow={1}
+            width="100%"
+            height="2rem"
+            sx={{
+              position: "absolute",
+              fontFamily: "Roboto",
+              textAlign: "left",
+              color: "text.primary",
+            }}
+          >
+            All our developments, whether for promotion, investment or for
+            immediate development are conceived with long term master-planning
+            and commitment in mind.
+          </Typography>
+        </Box>
+        <Box
+          style={{
+            position: "relative",
+            maxWidth: "80%",
+            margin: "auto",
+            marginBottom: "4rem",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "6rem",
+          }}
+        >
+          <Typography
+            component="div"
+            variant="body1"
+            flexGrow={1}
+            width="100%"
+            height="2rem"
+            sx={{
+              position: "absolute",
+              fontFamily: "Roboto",
+              textAlign: "left",
+              color: "text.primary",
+            }}
+          >
+            We aim to leave behind a legacy of architecture that will shape
+            minds and lives. We aim to inspire a generation using best in class
+            architects and innovation, leaving a legacy beyond just profit
+            margin.
+          </Typography>
+        </Box>
+        <Box
+          style={{
+            position: "relative",
+            maxWidth: "80%",
+            margin: "auto",
+            marginBottom: "-1.5rem",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "5rem",
+          }}
+        >
+          <Typography
+            component="div"
+            variant="h5"
+            flexGrow={1}
+            width="100%"
+            height="2rem"
+            sx={{
+              position: "absolute",
+              fontFamily: "Roboto",
+              textAlign: "center",
+              color: "text.primary",
+            }}
+          >
+            Influence an Experience
+          </Typography>
+        </Box>
+        <Box
+          style={{
+            position: "relative",
+            maxWidth: "80%",
+            margin: "auto",
+            marginBottom: "1rem",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "6rem",
+          }}
+        >
+          <Typography
+            component="div"
+            variant="body1"
+            flexGrow={1}
+            width="100%"
+            height="2rem"
+            sx={{
+              position: "absolute",
+              fontFamily: "Roboto",
+              textAlign: "left",
+              color: "text.primary",
+            }}
+          >
+            We Build long-term relationships with our partners & investors and
+            partner with leading specialists in each discipline across the
+            project lifecycle.
+          </Typography>
+        </Box>
+        <Box
+          style={{
+            position: "relative",
+            maxWidth: "80%",
+            margin: "auto",
+            marginBottom: "10rem",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "6rem",
+          }}
+        >
+          <Typography
+            component="div"
+            variant="body1"
+            flexGrow={1}
+            width="100%"
+            height="2rem"
+            sx={{
+              position: "absolute",
+              fontFamily: "Roboto",
+              textAlign: "left",
+              color: "text.primary",
+            }}
+          >
+            What sets Lydiard Capital apart is its target to sell entire phases
+            and portions of sites and developments off-plan with a view to
+            provide exemplar design credentials and quality of place, catering
+            to the needs of our customer base, this will include for example
+            providing homes and apartments that allow for flexible office
+            working with sustainable features. Providing a proven product that
+            stands out from its competitors.
+          </Typography>
+        </Box>
+        <Box
+          style={{
+            position: "relative",
+            maxWidth: "80%",
+            margin: "auto",
+            marginBottom: "1rem",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "6rem",
+          }}
+        >
+          <Typography
+            component="div"
+            variant="body1"
+            flexGrow={1}
+            width="100%"
+            height="2rem"
+            sx={{
+              position: "absolute",
+              fontFamily: "Roboto",
+              textAlign: "left",
+              color: "text.primary",
+            }}
+          >
+            At the heart of Lydiard Capital’s business model is based on a
+            long-term commitment to every site it takes an interest in, with a
+            drive to provide exemplar developments that leave a legacy to move
+            beyond just placemaking to truly influence an experience.
+          </Typography>
+        </Box>
       </Box>
     );
   }
