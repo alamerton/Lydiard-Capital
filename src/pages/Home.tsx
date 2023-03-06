570d8674dbae2a68a34cb6b88a9b8357d22769ca
AlfieLamerton <alfie2.lamerton@live.uwe.ac.uk>
2023-03-06 15:56:05 +0000

    Moved code from the landing page component to the home page and refactored container to render code properly

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,33 @@
 import React, { ReactElement, FC } from "react";
-import { Box } from "@mui/material";
-import LandingPage from "../components/LandingPage";
+import { Box, Container } from "@mui/material";
+import ConstructionCostsCard from "../components/ConstructionCostsCard";
+import ContactCard from "../components/ContactCard";
+import ExceptionalCard from "../components/ExceptionalCard";
+import GrossDevelopmentValueCard from "../components/GrossDevelopmentValueCard";
+import GroundBreakingVisionCard from "../components/GroundBreakingVisionCard";
+import InvestmentArmCard from "../components/InvestmentArmCard";
+import LandDevelopmentPromotionCard from "../components/LandDevelopmentPromotionCard";
+import SitesCard from "../components/SitesCard";
+import TopBanner from "../components/TopBanner";
+import TrueQualityLastsCard from "../components/TrueQualityLastsCard";
 
 const Home: FC<any> = (): ReactElement => {
   return (
     <Box
       sx={{
-        flexGrow: 1,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        marginBottom: "18rem",
       }}
     >
-      <LandingPage />
+      <TopBanner />
+      <GrossDevelopmentValueCard />
+      <ConstructionCostsCard />
+      <ExceptionalCard />
+      <GroundBreakingVisionCard />
+      <LandDevelopmentPromotionCard />
+      <InvestmentArmCard />
+      <SitesCard />
+      <TrueQualityLastsCard />
+      <ContactCard />
     </Box>
   );
 };
